76d4f2d0dcd7768edda707a62cfa32b7b1879fef
Anmol <anmoldeep20112003@gmail.com>
2025-09-30 20:12:42 +0530

    route

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,15 @@
-import './App.css'
-
+import { Route,Routes,Link,Navigate} from "react-router-dom"
+import Auth from "./pages/Auth"
+import Project from "./pages/Project"
+import IsLogin from "./util/IsLogin"
 const App = () => {
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Navigate to="/auth" />} />
+      <Route path="/auth" element={<Auth />}/>
+      <Route path="/:project" element={<IsLogin><Project /></IsLogin>}/>
+    </Routes>
     </>
   )
 }
